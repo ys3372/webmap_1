1f42caf961da609d52b7592cb4d02f343952ea55
Yining <ys3372@columbia.edu>
2022-03-01 11:55:23 -0500

    Update map.js

--- map.js
+++ map.js
@@ -65,3 +65,26 @@ var trees_url = 'data/2015_Street_Tree_Census_subset_um.geojson'
     });
 
     
+// when the user does a 'click' on an element in the 'trees' layer...
+map.on('click', 'trees', function(e) {
+  // get the map coordinates of the feature
+  var coordinates = e.features[0].geometry.coordinates.slice();
+  // get its species name from the feature's attributes
+  var species = e.features[0].properties.spc_common;
+
+  // and create a popup on the map
+  new mapboxgl.Popup()
+  .setLngLat(coordinates)
+  .setHTML(species)
+  .addTo(map);
+});
+  
+// make the cursor a pointer when over the tree
+map.on('mouseenter', 'trees', function() {
+  map.getCanvas().style.cursor = 'pointer';
+});
+  
+// back to normal when it's not
+map.on('mouseleave', 'trees', function() {
+  map.getCanvas().style.cursor = '';
+});
